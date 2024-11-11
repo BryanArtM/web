4dfdec81a74dc8c002f04630691861ccbea1c0fd
BryanArtM <barteagame@gmail.com>
2024-11-11 09:46:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -26,30 +26,29 @@
         </tr>
       </thead>
       <tbody>
-       <?php
+        <?php
 
-        $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "PRUEBA");
+            $conexion = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASSWORD'), "PRUEBA");
 
-        $cadenaSQL = "select * from s_cliente";
-        $resultado = mysqli_query($conexion, $cadenaSQL);
+            $cadenaSQL = "select * from s_cliente";
+            $resultado = mysqli_query($conexion, $cadenaSQL);
 
-        while ($fila = mysqli_fetch_object($resultado)) {
-         echo "<tr><td> " .$fila->nombre . 
-         "</td><td>" . $fila->calificacion_crediticia .
-         "</td><td>" . $fila->direccion .
-         "</td><td>" . $fila->ciudad .
-         "</td><td>" . $fila->provincia .
-         "</td><td>" . $fila->pais .
-         "</td><td>" . $fila->codigo_postal .
-         "</td></tr>";
-        }
-      ?>
+            while ($fila = mysqli_fetch_object($resultado)) {
+                echo "<tr><td> " .$fila->nombre . 
+                "</td><td>" . $fila->calificacion_crediticia .
+                "</td><td>" . $fila->direccion .
+                "</td><td>" . $fila->ciudad .
+                "</td><td>" . $fila->provincia .
+                "</td><td>" . $fila->pais .
+                "</td><td>" . $fila->codigo_postal .
+                "</td></tr>";
+            }
+        ?>
      </tbody>
    </table>
- </div>
+  </div>
  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 </body>
 </html>
-
